6e536c3bd87a10ed64b99674ffea8b97b61a5359
dilshan-shivantha <shiwanthadilshan3@gmail.com>
2023-12-16 01:16:58 +0530

    (04) little bit complex task

---
+++ Day 04/Code practice/04 code test/index.js
@@ -0,0 +1,14 @@
+//  import React from "react"
+// import ReactDOM from "react-dom";
+//
+// let root = document.getElementById("root")
+//
+// // how to use props as a functional parameter
+// let App = (prop) =>{
+//     return (
+//       <h1>dilsha</h1>
+//     );
+// }
+//
+//
+// ReactDOM.render(<App />,root)
